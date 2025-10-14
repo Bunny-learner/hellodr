e5bb961381db72c2387b5ad213c1b9b5b36cc8d0
Ambati Venkata Sai Tejesh <ambatikalyani1582@gmail.com>
2025-10-14 13:21:33 +0530

    Update auth.google.js

--- hellodr/hellodr/backend/middlewares/auth.google.js
+++ hellodr/hellodr/backend/middlewares/auth.google.js
@@ -12,7 +12,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:8000/auth/google/callback",
+      callbackURL: "https://hello-dr.onrender.com/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
       return done(null, profile);
