c0f8704ad93fdf6bdf2fd75af8c494735487a169
Ambati Venkata Sai Tejesh <ambatikalyani1582@gmail.com>
2025-10-14 13:04:35 +0530

    Update app.js

--- hellodr/hellodr/backend/app.js
+++ hellodr/hellodr/backend/app.js
@@ -17,7 +17,7 @@ import passport from "./middlewares/auth.google.js"
 
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://hello-dr.onrender.com", 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
